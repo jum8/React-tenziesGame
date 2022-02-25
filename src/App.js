325679f24cf380895a9de2c1778b89e5c2d275c5
jum8 <jumeight@gmail.com>
2022-02-24 21:32:04 -0300

    React Course - Beginner's Tutorial for React JavaScript Library\tenzies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  function randomDiceNumber() {
+    return Math.floor(6 * Math.random()) + 1;
+  }
+
+  function newDie() {
+    return {
+      value: Math.floor(6 * Math.random()) + 1,
+      isHeld: false,
+      id: nanoid(),
+    }
+  }
+
+  function allNewDice() {
+    const newDice = [];
+    for(let i = 0; i < 10; i++) {
+        newDice.push(newDie());
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die : newDie()
+    }))
+  }
+
+  function startNewGame() {
+    setTenzies(false);
+    setDice(allNewDice());
+  }
+  
+  function holdDice(id) {
+    setDice(prevDice => {
+      return prevDice.map(die => (die.id === id ? {...die, isHeld:!die.isHeld} : die));
+    })
+  }
+
+  function checkWinningConditions(diceArray) {
+    const firstValue = diceArray[0].value; 
+    for(let i = 0; i < diceArray.length; i++) {
+      const die = diceArray[i];
+      if(!die.isHeld || die.value !== firstValue) {
+        return false;
+      }  
+    }
+    return true;
+  }
+
+  React.useEffect(() =>{
+    if(checkWinningConditions(dice)) {
+      setTenzies(true);
+      console.log("You Won!");
+    }
+  }, [dice])
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button className="roll-dice" onClick={tenzies ? startNewGame : rollDice}>{tenzies ? "New game" : "Roll"}</button>
+    </main>
   );
 }
 
